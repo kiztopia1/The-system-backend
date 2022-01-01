1eb7a3760f6d56512d9a3d4eaeb4a4fb0c29128a
kirubel <kiztopia.2020@gmail.com>
2022-01-01 11:35:35 +0300

    nav added

---
+++ src/soldItemsPage/SoldItemsPage.js
@@ -0,0 +1,11 @@
+import React from 'react'
+import Nav from "../global/"
+function SoldItemsPage() {
+    return (
+        <div>
+            'boom'
+        </div>
+    )
+}
+
+export default SoldItemsPage
